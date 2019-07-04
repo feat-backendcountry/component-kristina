ceba071bfc1adf83d2e15214c4c02cf7383c951f
Kristina Taing <kataing12@gmail.com>
2019-07-04 13:28:55 -0700

    [feat] connected database and built product schema

---
+++ database/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/backcountry', { useNewUrlParser: true });
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Not connected to database:', err);
+  })
+
+const productsSchema = mongoose.Schema({
+  id: { type: Number, require: true },
+  name: { type: String, require: true },
+  url: { type: String, require: true},
+  category: { type: String, require: true},
+  brand: { type: String, require: true}
+});
+
+var Products = mongoose.model('Products', productsSchema);
+
+module.exports = Products;
