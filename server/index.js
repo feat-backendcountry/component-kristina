836047530fbc63d345f70a9bf5d73c7c903bd3ed
Kristina Taing <kataing12@gmail.com>
2019-07-04 13:26:35 -0700

    [feat] built server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3838;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(morgan('dev'));
+
+app.listen(port, () => console.log(`Connected and listening on port: ${port}`));
