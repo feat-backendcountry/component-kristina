06b65b4010e14822fa044fa9aa1fd654ad678531
Kristina Taing <kataing12@gmail.com>
2019-07-05 11:30:33 -0700

    [prog] seeded initial data with placehoder urls

---
+++ database/images.js
@@ -0,0 +1,144 @@
+const shirtsUrl = [
+  'shirt1',
+  'shirt2',
+  'shirt3',
+  'shirt4',
+  'shirt5',
+  'shirt6',
+  'shirt7',
+  'shirt8',
+  'shirt9',
+  'shirt10',
+  'shirt11',
+  'shirt12',
+  'shirt13',
+  'shirt14',
+  'shirt15'
+];
+
+const pantsUrl = [
+  'pants1',
+  'pants2',
+  'pants3',
+  'pants4',
+  'pants5',
+  'pants6',
+  'pants7',
+  'pants8',
+  'pants9',
+  'pants10',
+  'pants11',
+  'pants12',
+  'pants13',
+  'pants14',
+  'pants15'
+];
+
+const clothingUrl = [
+  'clothing1',
+  'clothing2',
+  'clothing3',
+  'clothing4',
+  'clothing5',
+  'clothing6',
+  'clothing7',
+  'clothing8',
+  'clothing9',
+  'clothing10',
+  'clothing11',
+  'clothing12',
+  'clothing13',
+  'clothing14'
+];
+
+const socksUrl = [
+  'socks1',
+  'socks2',
+  'socks3',
+  'socks4',
+  'socks5',
+  'socks6',
+  'socks7',
+  'socks8'
+];
+
+const shoesUrl = [
+  'shoes1',
+  'shoes2',
+  'shoes3',
+  'shoes4',
+  'shoes5',
+  'shoes6',
+  'shoes7',
+  'shoes8'
+];
+
+const glovesUrl = [
+  'gloves1',
+  'gloves2',
+  'gloves3',
+  'gloves4',
+  'gloves5',
+  'gloves6',
+  'gloves7',
+  'gloves8'
+];
+
+const helmetsUrl = [
+  'helmets1',
+  'helmets2',
+  'helmets3',
+  'helmets4',
+  'helmets5',
+  'helmets6',
+  'helmets7',
+  'helmets8'
+];
+
+const backpacksUrl = [
+  'backpacks1',
+  'backpacks2',
+  'backpacks3',
+  'backpacks4',
+  'backpacks5',
+  'backpacks6',
+  'backpacks7',
+  'backpacks8'
+];
+
+const jacketsUrl = [
+  'jackets1',
+  'jackets2',
+  'jackets3',
+  'jackets4',
+  'jackets5',
+  'jackets6',
+  'jackets7',
+  'jackets8'
+];
+
+const hoodiesUrl = [
+  'hoodies1',
+  'hoodies2',
+  'hoodies3',
+  'hoodies4',
+  'hoodies5',
+  'hoodies6',
+  'hoodies7',
+  'hoodies8'
+];
+
+const productTypes = {
+  'Shirt': shirtsUrl,
+  'Pant': pantsUrl,
+  'Clothing': clothingUrl,
+  'Sock': socksUrl,
+  'Shoe': shoesUrl,
+  'Glove': glovesUrl,
+  'Helmet': helmetsUrl,
+  'Backpack': backpacksUrl,
+  'Jacket': jacketsUrl,
+  'Hoodie': hoodiesUrl
+}
+
+module.exports = productTypes;
--- database/seed.js
+++ database/seed.js
@@ -1,10 +1,48 @@
 const mongoose = require('mongoose');
 const Product = require('./index.js');
 
+var productTypes = require('./images.js');
 
-const name = [];
-const url = [];
-const category = [
+var descriptions = [
+  'Atlantic TLS3',
+  'Icelandic ',
+  'Wilshire',
+  'Mosaic Trident',
+  'Peak Performance Odyssey',
+  'Hyper Blaze IC',
+  'Condor',
+  'Esk Forge',
+  'High Impact Toucan',
+  'Athena',
+  'Classic Athens',
+  'Soaring Balance',
+  'Serenity',
+  'Asterisk',
+  'Pinnacle Cove',
+  'Zip Discover',
+  'NextGen',
+  'OffRoad',
+  'Totem',
+  'Salamachu',
+  'Bolohock',
+  'Nompochock',
+  'Onehackhonhuh',
+  'Awkun',
+  'Chombriep',
+  'Suo',
+  'Igloo',
+  'NewWord',
+  'Solaris',
+  'Compact Retro',
+  'Atlas',
+  'Chameleon',
+  'ICIC',
+  'Vita Versus',
+  'Raven',
+  'Norwalk'
+];
+
+var categories = [
   'Men\'s Clothing',
   'Women\'s Clothing',
   'Men\'s Accessories',
@@ -12,41 +50,74 @@ const category = [
   'Bike',
   'Ski',
   'Snowboard',
-  'Tops',
-  'Pants',
-  'Baselayers',
+  'Baselayers'
 ];
-const brand = [
+
+// var childCategories = [
+//   'Bike',
+//   'Ski',
+//   'Snowboard',
+//   'Baselayers'
+// ];
+
+var brands = [
   'Arc\'teryx',
   'Black Diamond',
   'Backcountry',
   'Columbia',
-  'Clifbar',
   'Jade Yoga',
   'Keen',
+  'Marmot',
   'Mountain Hardwear',
   'Patagonia',
   'Prana',
   'The North Face'
 ];
 
-var seedData = []
-var entry = {};
+var seedData = [];
 
-var createSeedData = (name, url, category, brand) => {
-  for (var i = 1; i <= 100; i++) {
-    entry.id = i;
-      // entry.name = name[i];
-      // entry.url = url[i];
-    entry.category = category[Math.floor(Math.random() * (category.length - 1))];
-    entry.brandName = brand[Math.floor(Math.random() * (brand.length - 1))];
-    seedData.push(entry);
-    entry = {};
+var returnRandomIndex = (length) => {
+  return Math.floor(Math.random() * length);
+}
+
+var buildSeedData = (productTypes) => {
+
+  var makeItemEntries = (item, itemUrls) => {
+    for (var i = 0; i < itemUrls.length; i++) {
+      var entry = {};
+      // var nameExtension = '';
+
+      productType = `${item.toLowerCase()}s`;
+      category = categories[returnRandomIndex(categories.length - 1)];
+      // childCategory = childCategories[returnRandomIndex(childCategories.length - 1)]
+      brand = brands[returnRandomIndex(brands.length - 1)];
+      // nameExtension = category.split(' ')[0];
+      description = descriptions[returnRandomIndex(descriptions.length - 1)];
+
+      entry.id = id;
+      // entry.name = `${description} ${item} - ${nameExtension}`;
+      entry.name = `${description} ${item}`;
+      entry.url = itemUrls[i];
+      // entry.childCategory = childCategory;
+      entry.category = category;
+      entry.productType = productType
+      entry.brand = brand;
+
+      seedData.push(entry);
+      id++;
+    }
+  }
+
+  var id = 1;
+  var keys = Object.keys(productTypes);
+  for (var i = 0; i < keys.length; i++) {
+    makeItemEntries(keys[i], productTypes[keys[i]]);
   }
 }
 
 const seedFunction = () => {
-  Product.create(seedData)
+  Product
+    .create(seedData)
     .then(() => {
       console.log('Database seeded!');
       mongoose.connection.close();
@@ -54,5 +125,6 @@ const seedFunction = () => {
     .catch(err => console.error(err));
 }
 
-createSeedData(name, url, category, brand);
+buildSeedData(productTypes);
 seedFunction();
+// console.log(seedData);
