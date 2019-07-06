eb7151d0df657cbd8fa703952a37670e58dfff9d
Kristina Taing <kataing12@gmail.com>
2019-07-05 23:19:02 -0700

    [feat] added get request to send all db data to client

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const morgan = require('morgan');
-const Products = require('./index.js');
+const Products = require('../database/index.js');
 
 const app = express();
 const port = 3838;
@@ -11,5 +11,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(morgan('dev'));
+
+app.get('/products/all', (req, res) => {
+  Products
+    .find({})
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    })
+})
 
 app.listen(port, () => console.log(`Connected and listening on port: ${port}`));
