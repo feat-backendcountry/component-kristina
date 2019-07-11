0b0648589a6aba7ed8b72cb9c47d20829d364345
Kristina Taing <kataing12@gmail.com>
2019-07-11 15:16:14 -0700

    [feat] added on hover component activities

---
+++ client/hover-activities.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class HoverActivities extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mainActivities: ['Hike & Camp', 'Bike', 'Climb', 'Running & Training', 'Ski', 'Snowboard'],
+      otherActivities: ['Snowshoe', 'Paddle', 'Surf', 'FlyFish', 'Yoga', 'Travel']
+    };
+  };
+
+  render() {
+    return (
+      <section id='hover-activities'>
+          {this.state.mainActivities.map((activity, key) => {
+            return (
+              <span className='activity-container' key={key}>
+                <span className={'activity-image image-' + key}>
+                  <span className='activity-name'>{activity}</span>
+                </span>
+              </span>
+            )
+          })}
+        <div className='other-activities-container'>
+          {this.state.otherActivities.map((activity, key) => {
+            return(
+              <span className='other-activity-item' key={key}>{activity}</span>
+            )
+          })}
+        </div>
+      </section>
+
+    )
+  }
+};
+
+export default HoverActivities;
