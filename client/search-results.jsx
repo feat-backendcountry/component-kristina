e0079cfa2560a1feea3ede88c7555d6df9cca081
Kristina Taing <kataing12@gmail.com>
2019-07-16 16:24:52 -0700

    [prog] refactored html to map props for repetitive components

--- client/search-results.jsx
+++ client/search-results.jsx
@@ -8,18 +8,15 @@ const SearchResults = (props) => {
           <span className='query-text'>{props.query}</span>
           <span>remaining result</span>
         </span>
-        <span className='search-result-text-item'>
-          <span>{props.result[0].name}</span>
-          <span className='search-department'>{' in ' + props.result[0].childCategory}</span>
-        </span>
-        <span className='search-result-text-item'>
-          <span>{props.result[1].name}</span>
-          <span className='search-department'>{' in ' + props.result[1].childCategory}</span>
-        </span>
-        <span className='search-result-text-item'>
-          <span>{props.result[2].name}</span>
-          <span className='search-department'>{' in ' + props.result[2].childCategory}</span>
-        </span>
+        {props.searchTextItems.map((item, key) => {
+          return(
+            <span className='search-result-text-item' key={key}>
+              <span>{item.name.toLowerCase()}</span>
+              <span className='search-department'>{' in ' + item.childCategory}</span>
+            </span>
+          );
+        })}
+
         <span className='search-result-text-item'>----separator line-----</span>
         <span className='search-result-text-item'>
           <span className='query-text'>{props.query}</span>
@@ -59,47 +56,17 @@ const SearchResults = (props) => {
         </span>
       </div>
 
-      <div className='search-result-images-container'>
-        <span className='search-result-image-item'>
-          <img className='search-result-image' src={props.result[0].url}alt='search result 1' />
-          <span className='search-result-image-brand'>brand</span>
-          <span className='search-result-image-name'>item</span>
-        </span>
-        <span className='search-result-image-item'>
-          <img className='search-result-image' src={props.result[1].url}alt='search result 2' />
-          <span className='search-result-image-brand'>brand</span>
-          <span className='search-result-image-name'>item</span>
-        </span>
-        <span className='search-result-image-item'>
-          <img className='search-result-image' src={props.result[2].url}alt='search result 3' />
-          <span className='search-result-image-brand'>brand</span>
-          <span className='search-result-image-name'>item</span>
-        </span>
-        <span className='search-result-image-item'>
-          <img className='search-result-image' src={props.result[3].url}alt='search result 4' />
-          <span className='search-result-image-brand'>brand</span>
-          <span className='search-result-image-name'>item</span>
-        </span>
-        <span className='search-result-image-item'>
-          <img className='search-result-image' src={props.result[4].url}alt='search result 5' />
-          <span className='search-result-image-brand'>brand</span>
-          <span className='search-result-image-name'>item</span>
-        </span>
-        <span className='search-result-image-item'>
-          <img className='search-result-image' src={props.result[5].url}alt='search result 6' />
-          <span className='search-result-image-brand'>brand</span>
-          <span className='search-result-image-name'>item</span>
-        </span>
-        <span className='search-result-image-item'>
-          <img className='search-result-image' src={props.result[6].url}alt='search result 7' />
-          <span className='search-result-image-brand'>brand</span>
-          <span className='search-result-image-name'>item</span>
-        </span>
-        <span className='search-result-image-item'>
-          <img className='search-result-image' src={props.result[7].url}alt='search result 8' />
-          <span className='search-result-image-brand'>brand</span>
-          <span className='search-result-image-name'>item</span>
-        </span>
+      <div className='search-result-display-container'>
+        {props.searchDisplayItems.map((item, key) => {
+          return(
+            <span className='search-result-display-item' key={key}>
+              <img className='search-result-display' src={item.url}alt='search result 1' />
+              <span className='search-result-display-brand'>{item.brand}</span>
+              <span className='search-result-display-name'>{item.name}</span>
+            </span>
+          );
+        })
+        }
       </div>
     </section>
   )
