2d51c7d860d78aa5e4c964ee6278b732033ca750
Kristina Taing <kataing12@gmail.com>
2019-07-19 21:49:56 -0700

    [prog] added cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const morgan = require('morgan');
+const cors = require('cors');
 const Products = require('../database/index.js');
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(morgan('dev'));
 
-app.get('/products/all', (req, res) => {
+app.get('/products/all', cors(), (req, res) => {
   Products
     .find({})
     .then((data) => {
